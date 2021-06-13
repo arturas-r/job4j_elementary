173a5f5ed405f64dc0d9c312f99981db9d1c3e89
arturas-r <rudzenis.a@gmail.com>
2021-06-13 21:46:04 +0100

    0. Что такое тестирование [#159431]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -2,7 +2,8 @@ package ru.job4j.converter;
 
 public class Converter {
 
-    public static float rubleToEuro(float value) {
+   // old code
+    /* public static float rubleToEuro(float value) {
         //заменил int на float,
         // для того что бы программка показывала дробные числа
         return value / 70;
@@ -22,5 +23,35 @@ public class Converter {
         float dollar = Converter.rubleToDollar(440);
         // ради полноты эксперимента заменил 140 руб на 440
         System.out.println("440 rubles equal " + String.format("%.2f", dollar) + " dollars.");
+    } */
+
+    // new testing code
+    public static int rubleToEuro(int value) {
+        return value / 70;
+    }
+
+    public static int rubleToDollar(int value) {
+        return value / 60;
+    }
+
+    public static void main(String[] args) {
+        // ради эксперимента заменил 140 руб на 420
+        int euro = Converter.rubleToEuro(420);
+        System.out.println("420 rubles equal " + euro +  " euros.");
+
+        int inEuro = 420;
+        int expectedEuro = 6;
+        int outEuro = Converter.rubleToEuro(inEuro);
+        boolean passedEuro = expectedEuro == outEuro;
+        System.out.println("420 rubles equal 6. Test result : " + passedEuro);
+
+        int dollar = Converter.rubleToDollar(420);
+        System.out.println("420 rubles equal " + dollar + " dollars.");
+
+        int inDollar = 420;
+        int expectedDollar = 7;
+        int outDollar = Converter.rubleToDollar(inDollar);
+        boolean passedDollar = expectedDollar == outDollar;
+        System.out.println("420 rubles equal 7. Test result : " + passedDollar);
     }
 }
